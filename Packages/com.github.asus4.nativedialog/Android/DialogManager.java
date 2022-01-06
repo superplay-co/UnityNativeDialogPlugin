ef7069229eae0de202feff45cc22bbd7b0b84237
Vitalii Vodkevych <vitaliiv@superplay.co>
2022-01-06 18:04:02 +0200

    Added import

--- Packages/com.github.asus4.nativedialog/Android/DialogManager.java
+++ Packages/com.github.asus4.nativedialog/Android/DialogManager.java
@@ -5,6 +5,7 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.util.SparseArray;
 import com.google.android.material.dialog.MaterialAlertDialogBuilder;
+import android.util.Log;
 
 import com.unity3d.player.UnityPlayer;
 
